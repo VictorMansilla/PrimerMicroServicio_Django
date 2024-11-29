0de678d16a073cac37afe8b97099095afa6f9add
Victor Mansilla <Victormansilla.vrm@gmail.com>
2024-11-28 23:00:13 -0300

    Cuarto commit del microservicio

--- MicroServicio/AppServicio/urls.py
+++ MicroServicio/AppServicio/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import imagen_compresion, post_2
+from .views import imagen_compresion
 
 urlpatterns = [
     path('imagen_compresion/', imagen_compresion),
-    path('post_2/', post_2),
 ]
